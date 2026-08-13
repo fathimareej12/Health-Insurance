b4b63bca61138a1d14258cfef5f3e1b058425fe4
Areej <fathimareej12@gmail.com>
2026-08-13 16:57:18 +0400

    EDA module created

---
+++ src/components/eda.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def basic_eda(data:pd.DataFrame):
+    print('\n','='*50)
+    print('Exploratory data analysis'.center(50))
+    print('='*50)
+
+    print(f'Dataset shape: {data.shape}')
+    print('\n Target variable Distribution')
+    print(data['Response'].value_counts())
+
+    print('\n Numerical variable distribution')
+    print(data.describe())
+
+    print('\n Policy sales channel Distribution')
+    print(data['Policy_Sales_Channel'].value_counts())
+
+    print(pd.crosstab(data['Vehicle_Age'],data['Vehicle_Damage']))
+
+
+
+def main():
+    from data_ingestion import load_data
+
+    data=load_data()
+    basic_eda(data)
+
+if __name__ == '__main__':
+    main()
+
+
