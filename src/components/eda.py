60040a24caa16d043d783bcd7063ec96ae64126f
Areej <fathimareej12@gmail.com>
2026-08-17 17:26:15 +0400

    Saved EDA Results

--- src/components/eda.py
+++ src/components/eda.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import os
+
+Artifacts_path = "../../artifacts/reports"
+os.makedirs(Artifacts_path,exist_ok=True)
 
 def basic_eda(data:pd.DataFrame):
-    print('\n','='*50)
     print('Exploratory data analysis'.center(50))
-    print('='*50)
+    print('=' * 50)
 
     print(f'Dataset shape: {data.shape}')
     print('\n Target variable Distribution')
@@ -18,7 +21,27 @@ def basic_eda(data:pd.DataFrame):
     print('\n Age Distribution')
     print(data['Age'].value_counts())
 
-    print(pd.crosstab(data['Vehicle_Age'],data['Vehicle_Damage']))
+    print(pd.crosstab(data['Vehicle_Age'], data['Vehicle_Damage']))
+
+
+    with open('dataset_summary.txt','w')as file:
+        print('started')
+        file.write('\n Exploratory data analysis \n'.center(50))
+        file.write('=' * 50)
+        print('mid')
+
+        file.write(f'\n Dataset shape: {data.shape} \n')
+
+        file.write('\n Target variable Distribution \n')
+        file.write(str(data['Response'].value_counts()))
+
+        file.write('\n Policy sales channel Distribution \n')
+        file.write(str(data['Policy_Sales_Channel'].value_counts()))
+
+        file.write('\n Age Distribution\n')
+        file.write(str(data['Age'].value_counts()))
+
+
 
 
 
